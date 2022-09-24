44522af60255cc7f4bd572ba21765ea3212228ca
lin <2437706590@qq.com>
2022-09-24 16:07:10 +0800

    test push

--- PyLiao/网络编程.py
+++ PyLiao/网络编程.py
@@ -10,6 +10,8 @@ s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 s.connect(('www.sina.com',80))
 s.send(b'GET / HTTP/1.1\r\nHost:www.sina.com.cn\r\nConnection: close\r\n\r\n')
 
+
+
 buffer = []
 while True:
     d = s.recv(1024)
